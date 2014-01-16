95d3419ddcf83e95463a859e036202a734c8bb13
me <me@dc35e03b-bdef-4857-b32d-c0ba355d6291>
2014-01-16 16:43:42 +0000

    more integration work
    git-svn-id: file:///afs/csail.mit.edu/u/c/cwfletcher/REPOS/emulator_cores/tags/chip/oram@3186 dc35e03b-bdef-4857-b32d-c0ba355d6291

--- ORAM.v
+++ ORAM.v
@@ -2,39 +2,41 @@ module ORAM#
   (
    parameter MAX_ORAM_L = 32,
    parameter MAX_LOG_L = 5,
-   parameter ADDR_WIDTH = 30,
-   parameter DATA_WIDTH = 512
+   parameter DRAM_ADDR_WIDTH = 30,
+   parameter DRAM_DATA_WIDTH = 128,
+   parameter APP_ADDR_WIDTH = 64,
+   parameter APP_DATA_WIDTH = 64
    )
 
-   (
-    Clock,
-    Reset,
+    (
+     Clock,
+     Reset,
 
-    MIGRdy,
-    MIGEn, // enable memory controller
-    MIGInstr, // 0 for write, 1 for read
-    MIGAddr, // ORAM memory address
+     MIGRdy,
+     MIGEn, // enable memory controller
+     MIGInstr, // 0 for write, 1 for read
+     MIGAddr, // ORAM memory address
 
-    AppInstr, // application instruction
-    AppAddr, // addr from core
-    AppData, // data from core
+     AppInstr, // application instruction
+     AppAddr, // addr from core
+     AppData, // data from core
 
-    WrEn, // write enable
-    WrData, // write data
-    WrDataEnd, // indicate end of write
-    WrFull, // write fifo full
+     WrEn, // write enable
+     WrData, // write data
+     WrDataEnd, // indicate end of write
+     WrFull, // write fifo full
 
-    RdEn, // read enable
-    RdData, // read data
-    RdEmpty, // read fifo empty
+     RdEn, // read enable
+     RdData, // read data
+     RdEmpty, // read fifo empty
 
-    ORAMLevels,
-    L_st,
-    BktSize,
-    STSize,
-    NumCompST,
-    STSize_bot
-    );
+     ORAMLevels,
+     L_st,
+     BktSize,
+     STSize,
+     NumCompST,
+     STSize_bot
+     );
 
     local parameter AES_WIDTH = 128;
 
@@ -43,15 +45,15 @@ module ORAM#
     input                           MIGRdy;
 
     input                           AppInstr;
-    input [ADDR_WIDTH-1:0]          AppAddr;
-    input [DATA_WIDTH-1:0]          AppData;
+    input [APP_ADDR_WIDTH-1:0]      AppAddr;
+    input [APP_DATA_WIDTH-1:0]      AppData;
 
     output                          MIGEn;
     output [2:0]                    MIGInstr;
-    output [ADDR_WIDTH-1:0]         MIGAddr;
+    output [DRAM_ADDR_WIDTH-1:0]    MIGAddr;
 
     output                          WrEn;
-    output [DATA_WIDTH-1:0]         WrData;
+    output [APP_DATA_WIDTH-1:0]     WrData;
     output                          WrDataEnd;
 
     input                           WrFull;
@@ -68,29 +70,41 @@ module ORAM#
     input [MAX_ORAM_L-1:0]          STSize_bot;
 
 
-    wire                            Leaf; // comes from POSMAP
+    wire [MAX_ORAM_L-1:0]           Leaf; // comes from POSMAP
+    wire                            LeafValid;
 
     wire                            SymKey; // from key exchange
 
     wire                            OutLd; // indicate load
-    wire [DATA_WIDTH-1:0]           PlainIn; // plain text to AES
-    wire [DATA_WIDTH-1:0]           CipherOut; // data from AES
-    wire [DATA_WIDTH/AES_WIDTH-1:0] AESOutDone; // done encrypt
+    wire [APP_DATA_WIDTH-1:0]       PlainIn; // plain text to AES
+    wire [APP_DATA_WIDTH-1:0]       CipherOut; // data from AES
+    wire [APP_DATA_WIDTH/AES_WIDTH-1:0] AESOutDone; // done encrypt
 
-    wire                            InKld; // indicate key load
-    wire                            InLd; //indicate load
-    wire [DATA_WIDTH-1:0]           CipherIn; // plain text to AES
-    wire [DATA_WIDTH-1:0]           PlainOut; // data from AES
-    wire [DATA_WIDTH/AES_WIDTH-1:0] AESInDone;  // done decrypt
+    wire                                InKld; // indicate key load
+    wire                                InLd; //indicate load
+    wire [APP_DATA_WIDTH-1:0]           CipherIn; // plain text to AES
+    wire [APP_DATA_WIDTH-1:0]           PlainOut; // data from AES
+    wire [APP_DATA_WIDTH/AES_WIDTH-1:0] AESInDone;  // done decrypt
 
     wire                            AddrGenEn;
 
-    assign AddrGenEn = MIGRdy;
+    assign AddrGenEn = MIGRdy & LeafValid;
 
-    AddrGen#(.ADDR_WIDTH (ADDR_WIDTH)
+
+    PosMap#(.MAX_ORAM_L (MAX_ORAM_L),
+            .APP_ADDR_WIDTH (APP_ADDR_WIDTH))
+    posMap (.Clock(Clock),
+            .Reset(Reset),
+            .AppAddr(AppAddr),
+            .AppAddrValid(),
+            .Leaf(Leaf),
+            .LeafValid(LeafValid)
+            );
+
+
+    AddrGen#(.DRAM_ADDR_WIDTH (DRAM_ADDR_WIDTH)
              .MAX_ORAM_L (MAX_ORAM_L),
-             .MAX_LOG_L (MAX_LOG_L),
-             .DATA_WIDTH (DATA_WIDTH))
+             .MAX_LOG_L (MAX_LOG_L))
     addrGen (.Clock(Clock),
              .Reset(Reset),
              .Enable(AddrGenEn),
@@ -109,7 +123,7 @@ module ORAM#
 
     genvar                          i;
     generate
-        for (i = 0; i < DATA_WIDTH/AES_WIDTH; i = i + 1) begin: AES
+        for (i = 0; i < APP_DATA_WIDTH/AES_WIDTH; i = i + 1) begin: AES
             aes_cipher_top (.clk(Clock),
                             .rst(Reset),
                             .ld(OutLd),
