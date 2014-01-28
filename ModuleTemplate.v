96a40e2d6a37408e2a73468dd480ecf5ca16ab32
cwfletcher <cwfletcher@dc35e03b-bdef-4857-b32d-c0ba355d6291>
2014-01-28 03:44:42 +0000

    git-svn-id: file:///afs/csail.mit.edu/u/c/cwfletcher/REPOS/emulator_cores/tags/chip/oram@3241 dc35e03b-bdef-4857-b32d-c0ba355d6291

--- ModuleTemplate.v
+++ ModuleTemplate.v
@@ -21,6 +21,13 @@ module  #(/* parameters; e.g., `include "PathORAM.vh" */) (
 	//	Interface 1
 	//--------------------------------------------------------------------------
 		
+	// col5 // col 13			// col 33
+	input	[StashDWidth-1:0]	InData,
+	input	[ORAMU-1:0]			InPAddr,
+	input	[ORAML-1:0]			InLeaf,
+	input						InValid,
+	output 						InReady,
+	
 	//--------------------------------------------------------------------------
 	//	Interface 2
 	//--------------------------------------------------------------------------
@@ -32,17 +39,56 @@ module  #(/* parameters; e.g., `include "PathORAM.vh" */) (
 	//-------------------------------------------------------------------------- 
 		
 	// Local params; e.g., `include "DDR3SDRAMLocal.vh"	
-		
+	
+	// col 5				// col 29				// col 53
+	localparam				STWidth =				3,
+							ST_Reset =				3'd0,
+							ST_Idle = 				3'd1,
+							ST_Pushing = 			3'd2,
+							ST_Overwriting = 		3'd3, 
+							ST_Peaking =			3'd4, 
+							ST_Dumping =			3'd5; 
+	
 	//--------------------------------------------------------------------------
 	//	Wires & Regs
 	//-------------------------------------------------------------------------- 
 	
+	// col5 // col 13			// col 33
+	reg		[STWidth-1:0]		CS, NS;
+	wire	[StashEAWidth-1:0]	StashC_Address;
+	
 	//--------------------------------------------------------------------------
-	//	Rest of logic
+	//	State machines
 	//-------------------------------------------------------------------------- 
-
+	
+	// synchronous logic
+	always @(posedge Clock) begin
+		if (Reset) CS <= 							ST_Reset;
+		else CS <= 									NS;
+	end
+	
+	// combinational logic
+	always @( * ) begin
+		NS = 										CS;
+		case (CS)
+			ST_Reset : 
+				if (ResetDone) 
+					NS =						 	ST_Idle;
+			ST_Idle :
+				// blah blah
+		endcase
+	end	
+	
+	//--------------------------------------------------------------------------
+	//	Wire assignments
+	//-------------------------------------------------------------------------- 
+	
 	// col 5										// col 53
 	assign	myWire =								CoreResetDone & ScanTableResetDone;
+	
+	//--------------------------------------------------------------------------
+	//	Module instantiations
+	//-------------------------------------------------------------------------- 
 	
 	// col 5	// col 17	// col 29				// col 53
 	Counter		#(			.Width(					ScanTableAWidth),
