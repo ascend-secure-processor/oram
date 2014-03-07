23554e6dc77a4ac17314517a2d6971c0e480730e
renling <renling@dc35e03b-bdef-4857-b32d-c0ba355d6291>
2014-03-07 21:20:39 +0000

    git-svn-id: file:///afs/csail.mit.edu/u/c/cwfletcher/REPOS/emulator_cores/tags/chip/oram@3428 dc35e03b-bdef-4857-b32d-c0ba355d6291

--- testUORam.v
+++ testUORam.v
@@ -1,13 +1,13 @@
 `include "Const.vh"
 
+// `timescale 1ns/1ns
+
 module testUORam;
 						
 	parameter 					DDR_nCK_PER_CLK = 	4,
 								DDRDQWidth =		64,
 								DDRCWidth =			3,
-								DDRAWidth =			`log2(ORAMB * (ORAMZ + 1)) + ORAML + 1;
-								
-	parameter					StashCapacity =		100;
+								DDRAWidth =		`log2(ORAMB * (ORAMZ + 1)) + ORAML + 1;;
 	
 	parameter					IVEntropyWidth =	64;
 
@@ -25,96 +25,59 @@ module testUORam;
     reg [1:0] CmdIn;
     reg [ORAMU-1:0] AddrIn;
     wire [FEDWidth-1:0] DataIn, ReturnData;
+	
+	wire	[DDRCWidth-1:0]		DDR3SDRAM_Command;
+	wire	[DDRAWidth-1:0]		DDR3SDRAM_Address;
+	wire	[DDRDWidth-1:0]		DDR3SDRAM_WriteData, DDR3SDRAM_ReadData; 
+	wire	[DDRMWidth-1:0]		DDR3SDRAM_WriteMask;
+	
+	wire						DDR3SDRAM_CommandValid, DDR3SDRAM_CommandReady;
+	wire						DDR3SDRAM_WriteValid, DDR3SDRAM_WriteReady;
+	wire						DDR3SDRAM_ReadValid;
 
-    wire CmdOutReady, CmdOutValid;
-    wire [1:0] CmdOut;
-    wire [ORAMU-1:0] AddrOut;
-    wire [ORAML-1:0] OldLeaf, NewLeaf;
-    
-    wire StoreDataValid, LoadDataReady, StoreDataReady, LoadDataValid;
-    wire [FEDWidth-1:0] StoreData, LoadData;
-
-    UORamController #(  .ORAMU(         ORAMU), 
-                        .ORAML(         ORAML), 
-                        .ORAMB(         ORAMB), 
-                        .NumValidBlock( NumValidBlock), 
-                        .Recursion(     Recursion), 
-                        .LeafWidth(     LeafWidth), 
-                        .PLBCapacity(   PLBCapacity)) 
-    FrontEnd    (   .Clock(             Clock), 
-                    .Reset(             Reset), 
-                    .CmdInReady(        CmdInReady), 
-                    .CmdInValid(        CmdInValid), 
-                    .CmdIn(             CmdIn), 
-                    .ProgAddrIn(        AddrIn),
-                    .DataInReady(       DataInReady), 
-                    .DataInValid(       DataInValid), 
-                    .DataIn(            DataIn),                                    
-                    .ReturnDataReady(   ReturnDataReady), 
-                    .ReturnDataValid(   ReturnDataValid), 
-                    .ReturnData(        ReturnData),
+    PathORamTop        #(	.StopOnBlockNotFound(	0),
+                            .ORAMB(					ORAMB),
+                            .ORAMU(					ORAMU),
+                            .ORAML(					ORAML),
+                            .ORAMZ(					ORAMZ),
+                            .FEDWidth(				FEDWidth),
+                            .BEDWidth(				BEDWidth),							
+                            .DDR_nCK_PER_CLK(		DDR_nCK_PER_CLK),
+                            .DDRDQWidth(			DDRDQWidth),
+                            .DDRCWidth(				DDRCWidth),
+                            .DDRAWidth(				DDRAWidth),
+                            .IVEntropyWidth(		IVEntropyWidth),
+                            .NumValidBlock(         NumValidBlock), 
+                            .Recursion(             Recursion), 
+                            .LeafWidth(             LeafWidth), 
+                            .PLBCapacity(           PLBCapacity))
+            ORAM    (		.Clock(					Clock),
+                            .Reset(					Reset),
                             
-                    .CmdOutReady(       CmdOutReady), 
-                    .CmdOutValid(       CmdOutValid), 
-                    .CmdOut(            CmdOut), 
-                    .AddrOut(           AddrOut), 
-                    .OldLeaf(           OldLeaf), 
-                    .NewLeaf(           NewLeaf), 
-                    .StoreDataReady(    StoreDataReady), 
-                    .StoreDataValid(    StoreDataValid), 
-                    .StoreData(         StoreData),
-                    .LoadDataReady(     LoadDataReady), 
-                    .LoadDataValid(     LoadDataValid), 
-                    .LoadData(          LoadData)
-                );
-
-    // DRAM interface
-	wire	[DDRCWidth-1:0]		DRAM_Command;
-	wire	[DDRAWidth-1:0]		DRAM_Address;
-	wire	[DDRDWidth-1:0]		DRAM_WriteData, DRAM_ReadData; 
-	wire	[DDRMWidth-1:0]		DRAM_WriteMask;
-	wire						DRAM_CommandValid, DRAM_CommandReady;
-	wire						DRAM_WriteDataValid, DRAM_WriteDataReady;
-	wire						DRAM_ReadDataValid;	
-
-    PathORAMBackend #(		.StashCapacity(			StashCapacity),
-							.StopOnBlockNotFound(	0),
-							.ORAMB(					ORAMB),
-							.ORAMU(					ORAMU),
-							.ORAML(					ORAML),
-							.ORAMZ(					ORAMZ),
-							.FEDWidth(				FEDWidth),
-							.BEDWidth(				BEDWidth),							
-							.DDR_nCK_PER_CLK(		DDR_nCK_PER_CLK),
-							.DDRDQWidth(			DDRDQWidth),
-							.DDRCWidth(				DDRCWidth),
-							.DDRAWidth(				DDRAWidth),
-							.IVEntropyWidth(		IVEntropyWidth))
-            BackEnd(		.Clock(					Clock),
-							.Reset(					Reset),			
-							.Command(				CmdOut),
-							.PAddr(					AddrOut),
-							.CurrentLeaf(			OldLeaf),
-							.RemappedLeaf(			NewLeaf),
-							.CommandValid(			CmdOutValid),
-							.CommandReady(			CmdOutReady),
-							.LoadData(				LoadData),
-							.LoadValid(				LoadDataValid),
-							.LoadReady(				LoadDataReady),
-							.StoreData(				StoreData),
-							.StoreValid(			StoreDataValid),
-							.StoreReady(			StoreDataReady),
-							.DRAMCommandAddress(	DRAM_Address),
-							.DRAMCommand(			DRAM_Command),
-							.DRAMCommandValid(		DRAM_CommandValid),
-							.DRAMCommandReady(		DRAM_CommandReady),			
-							.DRAMReadData(			DRAM_ReadData),
-							.DRAMReadDataValid(		DRAM_ReadDataValid),			
-							.DRAMWriteData(			DRAM_WriteData),
-							.DRAMWriteMask(			DRAM_WriteMask),
-							.DRAMWriteDataValid(	DRAM_WriteDataValid),
-							.DRAMWriteDataReady(	DRAM_WriteDataReady));
-							
+                            // interface with network			
+                            .Cmd(				    CmdIn),
+                            .PAddr(					AddrIn),
+                            .CmdValid(			    CmdInValid),
+                            .CmdReady(			    CmdInReady),
+                            .DataInReady(           DataInReady), 
+                            .DataInValid(           DataInValid), 
+                            .DataIn(                DataIn),                                    
+                            .ReturnDataReady(       ReturnDataReady), 
+                            .ReturnDataValid(       ReturnDataValid), 
+                            .ReturnData(            ReturnData),
+                            
+                            // interface with DRAM		
+                            .DRAMAddress(           DDR3SDRAM_Address),
+                            .DRAMCommand(			DDR3SDRAM_Command),
+                            .DRAMCommandValid(		DDR3SDRAM_CommandValid),
+                            .DRAMCommandReady(		DDR3SDRAM_CommandReady),			
+                            .DRAMReadData(			DDR3SDRAM_ReadData),
+                            .DRAMReadDataValid(		DDR3SDRAM_ReadValid),			
+                            .DRAMWriteData(			DDR3SDRAM_WriteData),
+                            .DRAMWriteMask(			DDR3SDRAM_WriteMask),
+                            .DRAMWriteDataValid(	DDR3SDRAM_WriteValid),
+                            .DRAMWriteDataReady(	DDR3SDRAM_WriteReady));
+					
 	//--------------------------------------------------------------------------
 
 	//--------------------------------------------------------------------------
@@ -135,23 +98,21 @@ module testUORam;
 							.Initialized(			),
 							.PoweredUp(				),
 
-							.CommandAddress(		{DRAM_Address, 6'b000000}),
-							.Command(				DRAM_Command),
-							.CommandValid(			DRAM_CommandValid),
-							.CommandReady(			DRAM_CommandReady),
+							.CommandAddress(		{DDR3SDRAM_Address, 6'b000000}),
+							.Command(				DDR3SDRAM_Command),
+							.CommandValid(			DDR3SDRAM_CommandValid),
+							.CommandReady(			DDR3SDRAM_CommandReady),
 
-							.DataIn(				DRAM_WriteData),
-							.DataInMask(			DRAM_WriteMask),
-							.DataInValid(			DRAM_WriteDataValid),
-							.DataInReady(			DRAM_WriteDataReady),
+							.DataIn(				DDR3SDRAM_WriteData),
+							.DataInMask(			DDR3SDRAM_WriteMask),
+							.DataInValid(			DDR3SDRAM_WriteValid),
+							.DataInReady(			DDR3SDRAM_WriteReady),
 
-							.DataOut(				DRAM_ReadData),
+							.DataOut(				DDR3SDRAM_ReadData),
 							.DataOutErrorChecked(	),
 							.DataOutErrorCorrected(	),
-							.DataOutValid(			DRAM_ReadDataValid),
+							.DataOutValid(			DDR3SDRAM_ReadValid),
 							.DataOutReady(			1'b1));
-
-
 
     reg [64-1:0] CycleCount;
     initial begin
@@ -182,96 +143,95 @@ module testUORam;
                 TestCount,
                 cmd == 0 ? "Update" : cmd == 1 ? "Append" : cmd == 2 ? "Read" : "ReadRmv",
                 addr);
-            #(Cycle) CmdInValid <= 0;
+            #(Cycle + Cycle / 2) CmdInValid <= 0;
         end
     endtask
-
+    
     task Check_Leaf;
-        begin
-            $display("\t%s Block %d, \tLeaf %d --> %d", 
-                    CmdOut == 0 ? "Update" : CmdOut == 1 ? "Append" : CmdOut == 2 ? "Read" : "ReadRmv",
-                    AddrOut, CmdOut == 1 ? -1 : OldLeaf, NewLeaf);
-                
-                if (CmdOut == BECMD_Append) begin
-                    if (GlobalPosMap[AddrOut][ORAML]) begin
-                        $display("Error: appending existing Block %d", AddrOut);
-                        $finish;
-                    end
-                end
-                else if (GlobalPosMap[AddrOut][ORAML] == 0) begin
-                    $display("Error: requesting non-existing Block %d", AddrOut);
-                    $finish;               
-                end
-                else if (GlobalPosMap[AddrOut][ORAML-1:0] != OldLeaf) begin
-                    $display("Error: leaf label does not match, should be %d, %d provided", GlobalPosMap[AddrOut][ORAML-1:0], OldLeaf);
-                    $finish;              
-                end
-                   
-                GlobalPosMap[AddrOut] <= CmdOut == BECMD_ReadRmv ? 0 : {1'b1, NewLeaf};
-        end 
-    endtask    
-    
-    task Handle_ProgStore;
-        begin
-           #(Cycle);
-           DataInValid <= 1;
-           for (integer i = 0; i < FEORAMBChunks; i = i + 1) begin
-               while (!DataInReady)  #(Cycle);        
-               #(Cycle);
+       begin
+           $display("\t%s Block %d, \tLeaf %d --> %d", 
+                   ORAM.BEnd_Cmd == 0 ? "Update" : ORAM.BEnd_Cmd == 1 ? "Append" : ORAM.BEnd_Cmd == 2 ? "Read" : "ReadRmv",
+                   ORAM.BEnd_PAddr, ORAM.BEnd_Cmd == 1 ? -1 : ORAM.CurrentLeaf, ORAM.RemappedLeaf);
+               
+           if (ORAM.BEnd_Cmd == BECMD_Append) begin
+               if (GlobalPosMap[ORAM.BEnd_PAddr][ORAML]) begin
+                   $display("Error: appending existing Block %d", ORAM.BEnd_PAddr);
+                   $finish;
+               end
            end
-           DataInValid <= 0;
-        end
-    endtask
+           else if (GlobalPosMap[ORAM.BEnd_PAddr][ORAML] == 0) begin
+               $display("Error: requesting non-existing Block %d", ORAM.BEnd_PAddr);
+               $finish;               
+           end
+           else if (GlobalPosMap[ORAM.BEnd_PAddr][ORAML-1:0] != ORAM.CurrentLeaf) begin
+               $display("Error: leaf label does not match, should be %d, %d provided", GlobalPosMap[ORAM.BEnd_PAddr][ORAML-1:0], ORAM.CurrentLeaf);
+               $finish;              
+           end
+              
+           GlobalPosMap[ORAM.BEnd_PAddr] <= ORAM.BEnd_Cmd == BECMD_ReadRmv ? 0 : {1'b1, ORAM.RemappedLeaf};
+       end 
+    endtask    
+   
+   task Handle_ProgStore;
+       begin
+          #(Cycle);
+          DataInValid <= 1;
+          #(Cycle / 2)
+          for (integer i = 0; i < FEORAMBChunks; i = i + 1) begin
+              while (!DataInReady)  #(Cycle);        
+              #(Cycle);
+          end
+          DataInValid <= 0;
+       end
+   endtask
     
-    
-    
-    reg  [ORAMU-1:0] AddrRand;
-    wire [1:0] Op;
-    wire  Exist;
-    
-    assign Exist = GlobalPosMap[AddrRand][ORAML];
-    assign Op = Exist ? {GlobalPosMap[AddrRand][0], 1'b0} : 2'b00;
-    
-    initial begin
-        TestCount <= 0;
-        CmdInValid <= 0;
-        DataInValid <= 0;
-        ReturnDataReady <= 1;   
-        AddrRand <= 0;
-          
-        for (integer i = 0; i < TotalNumBlock; i=i+1) begin
-            GlobalPosMap[i][ORAML] <= 0;
-        end         
-    end
-    
-    wire WriteCmd;
-    assign WriteCmd = CmdOut == BECMD_Append || CmdOut == BECMD_Update;
-    
-    always @(posedge Clock) begin
-        if (CmdInReady) begin
-            if (TestCount < 200) begin
-                Task_StartORAMAccess(Op, AddrRand);
-                #(Cycle);       
-                AddrRand <= ($random % (NumValidBlock / 2)) + NumValidBlock / 2;
-                TestCount <= TestCount + 1;
-            end
-            else begin
-                $display("ALL UORAM TESTS PASSED!");
-                $finish;  
-            end
-        end
-    end
-    
-    always @(posedge Clock) begin
-        if (CmdOutValid && CmdOutReady && WriteCmd && AddrOut < NumValidBlock) begin
-            Handle_ProgStore;
-        end
-    end
-    
+   reg  [ORAMU-1:0] AddrRand;
+   wire [1:0] Op;
+   wire  Exist;
+   
+   assign Exist = GlobalPosMap[AddrRand][ORAML];
+   assign Op = Exist ? {GlobalPosMap[AddrRand][0], 1'b0} : 2'b00;
+   
+   initial begin
+       TestCount <= 0;
+       CmdInValid <= 0;
+       DataInValid <= 0;
+       ReturnDataReady <= 1;   
+       AddrRand <= 0;
+         
+       for (integer i = 0; i < TotalNumBlock; i=i+1) begin
+           GlobalPosMap[i][ORAML] <= 0;
+       end         
+   end
+   
+   wire WriteCmd;
+   assign WriteCmd = CmdIn == BECMD_Append || CmdIn == BECMD_Update;
+   
+   always @(posedge Clock) begin
+       if (CmdInReady) begin
+           if (TestCount < 2000) begin
+               Task_StartORAMAccess(Op, AddrRand);
+               #(Cycle);       
+               AddrRand <= ($random % (NumValidBlock / 2)) + NumValidBlock / 2;
+               TestCount <= TestCount + 1;
+           end
+           else begin
+               $display("FULL ORAM TESTS PASSED!");
+               $finish;  
+           end
+       end
+   end
+   
+   always @(posedge Clock) begin
+       if (CmdInValid && CmdInReady && WriteCmd) begin
+           Handle_ProgStore;
+       end
+   end
+   
     always @(posedge Clock) begin    
-        if (CmdOutValid && CmdOutReady) begin
-            Check_Leaf;
-        end
-    end
-    
+       if (ORAM.BEnd_CmdValid && ORAM.BEnd_CmdReady) begin
+           Check_Leaf;
+       end
+   end
+       
 endmodule
