e614acf01b1b5e85cc03f2c7b6d9725f243e9a3e
cwfletcher <cwfletcher@dc35e03b-bdef-4857-b32d-c0ba355d6291>
2014-04-16 20:34:18 +0000

    git-svn-id: file:///afs/csail.mit.edu/u/c/cwfletcher/REPOS/emulator_cores/tags/chip/oram@3641 dc35e03b-bdef-4857-b32d-c0ba355d6291

---
+++ aes/aes_clock/aes_clock_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-1999, 2001-2013 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2013.4 (win64) Build 353583 Mon Dec  9 17:49:19 MST 2013
+// Date        : Mon Apr 07 16:59:50 2014
+// Host        : running 64-bit Service Pack 1  (build 7601)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/chip/tiny_aes_example/tiny_aes_example.srcs/sources_1/ip/aes_clock/aes_clock_stub.v
+// Design      : aes_clock
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7vx485tffg1761-2
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module aes_clock(clk_in1, clk_out1, reset, locked)
+/* synthesis syn_black_box black_box_pad_pin="clk_in1,clk_out1,reset,locked" */;
+  input clk_in1;
+  output clk_out1;
+  input reset;
+  output locked;
+endmodule
