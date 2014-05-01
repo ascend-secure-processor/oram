db36e1222c3b9ab9c8007bf212bc53bff292f5b8
me <me@dc35e03b-bdef-4857-b32d-c0ba355d6291>
2014-05-01 01:44:52 +0000

    axi_oram edits
    git-svn-id: file:///afs/csail.mit.edu/u/c/cwfletcher/REPOS/emulator_cores/tags/chip/oram@3746 dc35e03b-bdef-4857-b32d-c0ba355d6291

--- aes/rew/GentrySeedGenerator.v
+++ aes/rew/GentrySeedGenerator.v
@@ -7,38 +7,38 @@
 
 //==============================================================================
 //	Module:		GentrySeedGenerator
-//	Desc:		
+//	Desc:
 //==============================================================================
 module GentrySeedGenerator(
   	Clock, Reset,
-	
-	OutIV, OutBID, 
+
+	OutIV, OutBID,
 	OutValid, OutReady
 	);
-	
+
 	//--------------------------------------------------------------------------
 	//	Constants
 	//--------------------------------------------------------------------------
 
 	parameter				ORAML = 0;
-	
+
 	`include "SecurityLocal.vh"
-	
+
 	// TODO We only need these includes because we need BIDWidth in REWAESLocal.vh
 	// Clean up param system to avoid the crazy includes ...
-//	`include "PathORAM.vh"
-//	`include "DDR3SDRAMLocal.vh"
-//	`include "BucketDRAMLocal.vh"
-//	`include "REWAESLocal.vh"
+//	`include "PathORAM.v h"
+//	`include "DDR3SDRAMLocal.v h"
+//	`include "BucketDRAMLocal.v h"
+//	`include "REWAESLocal.v h"
 
 	localparam				BIDWidth = ORAML + 1;
-							
+
 	//--------------------------------------------------------------------------
 	//	System I/O
 	//--------------------------------------------------------------------------
-		
+
   	input 					Clock, Reset;
-	
+
 	//--------------------------------------------------------------------------
 	//	Output Interface
 	//--------------------------------------------------------------------------
@@ -48,49 +48,49 @@ module GentrySeedGenerator(
 
 	output					OutValid;
 	input					OutReady;
-	
+
 	//--------------------------------------------------------------------------
 	//	Wires & Regs
-	//-------------------------------------------------------------------------- 
+	//--------------------------------------------------------------------------
 	wire					RWPathTransition;
 	wire					Transfer;
 
 	wire	[AESEntropy-1:0] GentryCounter, GentryCounterShifted, GentryCounterIn;
-	
+
 	wire	[ORAML-1:0]		GentryLeaf;
-	
+
 	//--------------------------------------------------------------------------
 	//	Logic
-	//-------------------------------------------------------------------------- 	
-	
+	//--------------------------------------------------------------------------
+
 	reg CSRWWrite, CSStartOp;
-	
+
 	`ifndef ASIC
-		initial begin	
+		initial begin
 			CSStartOp <= 1;
 			CSRWWrite <= 0;	// read first
 		end
 	`endif
-	
+
 	always @(posedge Clock) begin
 		if (Reset) begin
 			CSStartOp <= 1;
 			CSRWWrite <= 0;	// read first
-		end 
+		end
 		else begin
-			if (CSStartOp) 
+			if (CSStartOp)
 				CSStartOp <= 0;
-			if (RWPathTransition) 
+			if (RWPathTransition)
 				CSRWWrite <= ~CSRWWrite;
 		end
 	end
-	
+
 	CountAlarm #(			.Threshold(				ORAML + 1))
-				rw_lvl_cnt(	.Clock(					Clock), 
-							.Reset(					Reset), 
+				rw_lvl_cnt(	.Clock(					Clock),
+							.Reset(					Reset),
 							.Enable(				Transfer),
 							.Done(					RWPathTransition));
-	
+
 	Counter		#(			.Width(					AESEntropy))
 				gentry_bg(	.Clock(					Clock),
 							.Reset(					Reset),
@@ -98,26 +98,26 @@ module GentrySeedGenerator(
 							.Load(					1'b0),
 							.Enable(				RWPathTransition & CSRWWrite),
 							.In(					{AESEntropy{1'bx}}),
-							.Count(					GentryCounter));						
-							
+							.Count(					GentryCounter));
+
 	// RW seed generation scheme for bucket @ level L (L = 0...):
 	//	decrypt( GentryCounter >> L)
 	//	encrypt((GentryCounter >> L) + 1)
 	ShiftRegister #(		.PWidth(				AESEntropy),
 							.Reverse(				1),
 							.SWidth(				1))
-				gentry_shft(.Clock(					Clock), 
-							.Reset(					1'b0), 
+				gentry_shft(.Clock(					Clock),
+							.Reset(					1'b0),
 							.Load(					RWPathTransition || CSStartOp),
-							.Enable(				Transfer), 
+							.Enable(				Transfer),
 							.PIn(					GentryCounterIn),
 							.SIn(					1'b0),
 							.POut(					GentryCounterShifted));
-		
+
 	assign  GentryCounterIn = 		GentryCounter + (RWPathTransition & CSRWWrite);
-	
+
 	assign	OutIV =					GentryCounterShifted + CSRWWrite;
-	
+
 	assign	GentryLeaf =			GentryCounterIn[ORAML-1:0];
 
 	BktIDGen # 	(	.ORAML(		ORAML))
@@ -125,12 +125,12 @@ module GentrySeedGenerator(
 					.ReStart(	CSStartOp || RWPathTransition),
 					.leaf(		GentryLeaf),
 					.Enable(	Transfer),
-					.BktIdx(	OutBID)			
+					.BktIdx(	OutBID)
 				);
 
-	assign	OutValid = !CSStartOp;					
+	assign	OutValid = !CSStartOp;
 	assign	Transfer = OutValid && OutReady;
-	
+
 	//--------------------------------------------------------------------------
 endmodule
 //--------------------------------------------------------------------------
