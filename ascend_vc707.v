555776f7200eb7e15cb0f824d833afbd9db5f0da
cwfletcher <cwfletcher@dc35e03b-bdef-4857-b32d-c0ba355d6291>
2014-05-26 16:37:50 +0000

    DRAM wrapper
    -- vc707 top is not stable right now --
    
    git-svn-id: file:///afs/csail.mit.edu/u/c/cwfletcher/REPOS/emulator_cores/tags/chip/oram@3989 dc35e03b-bdef-4857-b32d-c0ba355d6291

--- ascend_vc707.v
+++ ascend_vc707.v
@@ -72,7 +72,7 @@ module ascend_vc707(
 	// CCS paper configurations
 	parameter				UnifiedExperiment =		1;
 	parameter				REWExperiment =			1;
-	parameter				REWIVExperiment =		`ifdef EnableIV 			`EnableIV 			`else 1 `endif;
+	parameter				REWIVExperiment =		`ifdef EnableIV `EnableIV `else 1 `endif;
 	
 	// ORAM related
 	
@@ -89,7 +89,7 @@ module ascend_vc707(
     parameter				NumValidBlock = 		1 << ORAML,
 							Recursion = 			3,
 							EnablePLB = 			UnifiedExperiment,
-							PLBCapacity = 			`ifdef PLBCapacity `PLBCapacity `else 8192 << 3 `endif;
+							PLBCapacity = 			`ifdef PLBCapacity `PLBCapacity `else 8192 << 3 `endif; // 8KB PLB
 		
 	parameter				Overclock =				1;
 	
@@ -139,17 +139,13 @@ module ascend_vc707(
 	
 	// Clocking
 	
-	wire					MemoryClock; // always 200 Mhz (matches MIG)
-	wire					MemoryReset_Pre;
-	reg						MemoryReset;
-	
 	wire					ORAMClock; // Configurable (typically >= 100 Mhz, <= 200 Mhz)
 	wire					ORAMReset;
 	
-	wire					AESClock; // As fast as possible (~300 Mhz)
-	
 	wire					SlowClock;
 	wire					MMCMF100Locked, SlowReset;
+
+	wire					AESClock; // As fast as possible (~300 Mhz)	
 	
 	// Test harness
 	
@@ -170,7 +166,7 @@ module ascend_vc707(
 	
 	// MIG/DDR3 DRAM
 	
-	wire					DDR3SDRAM_ResetDone;
+	wire					DRAMCalibrationComplete;
 	
 	(* mark_debug = "TRUE" *)	wire	[DDRCWidth-1:0]	DDR3SDRAM_Command;
 	(* mark_debug = "TRUE" *)	wire	[DDRAWidth_Top-1:0]	DDR3SDRAM_Address;
@@ -180,35 +176,26 @@ module ascend_vc707(
 	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_CommandValid, DDR3SDRAM_CommandReady;
 	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_DataInValid, DDR3SDRAM_DataInReady;
 	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_DataOutValid;
-
-	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_CommandValid_MIG_Pre, DDR3SDRAM_DataInValid_MIG_Pre;
-	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_CommandReady_MIG_Pre, DDR3SDRAM_DataInReady_MIG_Pre;
-	
-	(* mark_debug = "TRUE" *)	wire	[DDRCWidth-1:0]	DDR3SDRAM_Command_MIG;
-	(* mark_debug = "TRUE" *)	wire	[DDRAWidth_Top-1:0]	DDR3SDRAM_Address_MIG;
-	(* mark_debug = "TRUE" *)	wire	[DDRDWidth-1:0]	DDR3SDRAM_WriteData_MIG, DDR3SDRAM_ReadData_MIG; 
-	wire	[DDRMWidth-1:0]	DDR3SDRAM_WriteMask_MIG;
-
-	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_CommandValid_MIG, DDR3SDRAM_CommandReady_MIG;
-	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_DataInValid_MIG, DDR3SDRAM_DataInReady_MIG;
-	(* mark_debug = "TRUE" *)	wire					DDR3SDRAM_DataOutValid_MIG;	
-		
-	(* mark_debug = "TRUE" *)	wire					PathWriteback;
 		
 	//------------------------------------------------------------------------------
 	// 	Clocking
 	//------------------------------------------------------------------------------
  
-	F100ClockGen clk_div_2(	.clk_in1(				MemoryClock),
+	generate if (SlowORAMClock) begin:SLOW_ORAM
+		assign	SlowClock =							ORAMClock;
+		assign	SlowReset =							ORAMReset;
+	end else begin:FAST_ORAM
+		F100ClockGen cd2(	.clk_in1(				ORAMClock),
 							.clk_out1(				SlowClock),
 							.reset(					MemoryReset),
 							.locked(				MMCMF100Locked));
-	assign	SlowReset =								~MMCMF100Locked;
-
+		assign	SlowReset =							~MMCMF100Locked;
+	end endgenerate
+	
 	generate if (SlowAESClock) begin:SLOW_AES
 		assign	AESClock =							SlowClock;
 	end else begin:FAST_AES
-		aes_clock	ultra( 	.clk_in1(				MemoryClock),
+		aes_clock	ci15( 	.clk_in1(				ORAMClock),
 							.clk_out1(				AESClock),
 							.reset(					MemoryReset),
 							.locked(				));
@@ -223,7 +210,7 @@ module ascend_vc707(
 		Tester_ForceHistogramDump <=				Tester_ForceHistogramDumpPre;
 	end	
 
-	assign	led[7] =								DDR3SDRAM_ResetDone;
+	assign	led[7] =								DRAMCalibrationComplete;
 	assign	led[6] = 								UnifiedExperiment;
 	assign	led[5] = 								REWExperiment;
 	assign	led[4] = 								REWIVExperiment;
@@ -338,194 +325,46 @@ module ascend_vc707(
 							.DRAMWriteDataReady(	DDR3SDRAM_DataInReady));
 	
 	//------------------------------------------------------------------------------
-	//	Debugging clock crossings
-	//------------------------------------------------------------------------------	
-
-	generate if (SlowORAMClock) begin:SLOW_ORAM	
-		wire				CommandBuf_Full, WriteDataBuf_Full;
-	
-		assign	ORAMClock =							SlowClock;
-		assign	ORAMReset =							SlowReset;
-		
-		assign	DDR3SDRAM_CommandReady = 			~CommandBuf_Full;
-		DebugCommandFIFO dcmd(	.rst(				ORAMReset),
-								.wr_clk(			ORAMClock),
-								.rd_clk(			MemoryClock),
-								
-								.din(				{DDR3SDRAM_Command, 	DDR3SDRAM_Address}),
-								.wr_en(				DDR3SDRAM_CommandValid),
-								.full(				CommandBuf_Full),
-								
-								.dout(				{DDR3SDRAM_Command_MIG, DDR3SDRAM_Address_MIG}),
-								.rd_en(				DDR3SDRAM_CommandReady_MIG_Pre),
-								.valid(				DDR3SDRAM_CommandValid_MIG_Pre));
-
-		assign	DDR3SDRAM_DataInReady = 			~WriteDataBuf_Full;
-		DebugDataWFIFO dwr(		.rst(				ORAMReset),
-								.wr_clk(			ORAMClock),
-								.rd_clk(			MemoryClock),
-								
-								.din(				{DDR3SDRAM_WriteMask, 		DDR3SDRAM_WriteData}),
-								.wr_en(				DDR3SDRAM_DataInValid),
-								.full(				WriteDataBuf_Full),
-								
-								.dout(				{DDR3SDRAM_WriteMask_MIG, 	DDR3SDRAM_WriteData_MIG}),
-								.rd_en(				DDR3SDRAM_DataInReady_MIG_Pre),
-								.valid(				DDR3SDRAM_DataInValid_MIG_Pre));
-		
-		DebugDataFIFO drd(		.rst(				ORAMReset),
-								.wr_clk(			MemoryClock),
-								.rd_clk(			ORAMClock),
-								.din(				DDR3SDRAM_ReadData_MIG),
-								.wr_en(				DDR3SDRAM_DataOutValid_MIG),
-								.rd_en(				1'b1),
-								.dout(				DDR3SDRAM_ReadData),
-								.full(				),
-								.valid(				DDR3SDRAM_DataOutValid));	
-	end else begin:FAST_ORAM
-		assign	ORAMClock =							MemoryClock;
-		assign	ORAMReset =							MemoryReset;
-		
-		assign	DDR3SDRAM_Command_MIG =				DDR3SDRAM_Command;
-		assign	DDR3SDRAM_Address_MIG =				DDR3SDRAM_Address;
-		assign	DDR3SDRAM_CommandValid_MIG_Pre =	DDR3SDRAM_CommandValid;
-		assign	DDR3SDRAM_CommandReady = 			DDR3SDRAM_CommandReady_MIG_Pre;
-		
-		assign	DDR3SDRAM_WriteData_MIG =			DDR3SDRAM_WriteData;
-		assign	DDR3SDRAM_WriteMask_MIG =			DDR3SDRAM_WriteMask;
-		assign	DDR3SDRAM_DataInValid_MIG_Pre =		DDR3SDRAM_DataInValid;
-		assign	DDR3SDRAM_DataInReady = 			DDR3SDRAM_DataInReady_MIG_Pre;
-		
-		assign	DDR3SDRAM_ReadData =				DDR3SDRAM_ReadData_MIG;
-		assign	DDR3SDRAM_DataOutValid = 			DDR3SDRAM_DataOutValid_MIG;
-	end endgenerate
-	
-	//------------------------------------------------------------------------------
-	//	Join command & write interface
-	//------------------------------------------------------------------------------	
-	
-	// This is needed only because MIG is bugged and will drop write data if we 
-	// present WriteCommands & WriteData out of sync with each other
-	// NOTE: this workaround doesn't impact writeback performance
-	
-	assign	PathWriteback =							(DDR3SDRAM_Command_MIG == DDR3CMD_Write) & DDR3SDRAM_CommandValid_MIG_Pre;
-	
-	assign	DDR3SDRAM_CommandValid_MIG =			((PathWriteback) ? 	DDR3SDRAM_DataInValid_MIG_Pre & DDR3SDRAM_DataInReady_MIG : 1'b1) & 	DDR3SDRAM_CommandValid_MIG_Pre;
-	assign	DDR3SDRAM_DataInValid_MIG =				PathWriteback & 																			DDR3SDRAM_CommandValid_MIG_Pre & DDR3SDRAM_DataInValid_MIG_Pre & DDR3SDRAM_CommandReady_MIG;
-	
-	assign	DDR3SDRAM_CommandReady_MIG_Pre =		((PathWriteback) ?	DDR3SDRAM_DataInValid_MIG_Pre &	DDR3SDRAM_DataInReady_MIG : 1'b1) & 	DDR3SDRAM_CommandReady_MIG;
-	assign	DDR3SDRAM_DataInReady_MIG_Pre =			PathWriteback & 																			DDR3SDRAM_CommandReady_MIG & DDR3SDRAM_CommandValid_MIG_Pre & DDR3SDRAM_DataInReady_MIG;
-	
-	//------------------------------------------------------------------------------
-	//	DDR3SDRAM (MIG7 or some synthetic memory)
+	//	DRAM Controller
 	//------------------------------------------------------------------------------
 	
-	always @(posedge MemoryClock) begin
-		MemoryReset <=								MemoryReset_Pre;						
-	end
-	
-	`ifdef SIMULATION
-	
-	// -------------------------------------------------------------------------
-	//	Fake MIG
-	// -------------------------------------------------------------------------
-	
-	wire					MemoryClock_Bufg;
-	IBUFGDS	clk_f200_p(		.I(						sys_clk_p),
-							.IB(					sys_clk_n),
-							.O(						MemoryClock_Bufg));
-	BUFG 	clk_f200(		.I(						MemoryClock_Bufg),
-							.O(						MemoryClock));
-	assign	MemoryReset_Pre =						sys_rst;
-
-	assign	DDR3SDRAM_ResetDone =					~MemoryReset;
-	
-	SynthesizedRandDRAM	#(	.InBufDepth(			6), // Set to match MIG7
-							.InDataBufDepth(		16), // Set to match MIG7
-	                        .OutInitLat(			25), // Set to match MIG7
-	                        .OutBandWidth(			100), // Set to match MIG7
-							.UWidth(				DDRDQWidth),
-							.AWidth(				DDRAWidth_Top),
-							.DWidth(				DDRDWidth),
-							.BurstLen(				1),
-							.EnableMask(			1),
-							.Class1(				1),
-							.RLatency(				1),
-							.WLatency(				1))
-				fake_mig(	.Clock(					MemoryClock),
-							.Reset(					MemoryReset),
-
-							.CommandAddress(		DDR3SDRAM_Address_MIG),
-							.Command(				DDR3SDRAM_Command_MIG),
-							.CommandValid(			DDR3SDRAM_CommandValid_MIG),
-							.CommandReady(			DDR3SDRAM_CommandReady_MIG),
-
-							.DataIn(				DDR3SDRAM_WriteData_MIG),
-							.DataInMask(			DDR3SDRAM_WriteMask_MIG),
-							.DataInValid(			DDR3SDRAM_DataInValid_MIG),
-							.DataInReady(			DDR3SDRAM_DataInReady_MIG),
-
-							.DataOut(				DDR3SDRAM_ReadData_MIG),
-							.DataOutValid(			DDR3SDRAM_DataOutValid_MIG),
-							.DataOutReady(			1'b1));
-	`else
-	
-	// -------------------------------------------------------------------------
-	//	Real MIG
-	// -------------------------------------------------------------------------
-	
-	// We put MIG here so that the constraint file doesn't need to be changed
-	
-	DDR3SDRAM DDR3SDRAMController(
-							// System interface
-							.sys_clk_p(				sys_clk_p),
-							.sys_clk_n(				sys_clk_n),
-							.sys_rst(				sys_rst),
-  							.ui_clk(				MemoryClock),
-							.ui_clk_sync_rst(		MemoryReset_Pre),
-							.init_calib_complete(	DDR3SDRAM_ResetDone),
-														
-							// DDR3 interface
-							.ddr3_addr(				ddr3_addr),
-							.ddr3_ba(				ddr3_ba),
-							.ddr3_cas_n(			ddr3_cas_n),
-							.ddr3_ck_n(				ddr3_ck_n),
-							.ddr3_ck_p(				ddr3_ck_p),
-							.ddr3_cke(				ddr3_cke),
-							.ddr3_ras_n(			ddr3_ras_n),
-							.ddr3_reset_n(			ddr3_reset_n),
-							.ddr3_we_n(				ddr3_we_n),
-							.ddr3_dq(				ddr3_dq),
-							.ddr3_dqs_n(			ddr3_dqs_n),
-							.ddr3_dqs_p(			ddr3_dqs_p),
-							.ddr3_cs_n(				ddr3_cs_n),
-							.ddr3_dm(				ddr3_dm),
-							.ddr3_odt(				ddr3_odt),
+	DDR3SDRAMTop #(			.SlowUserClock(			SlowORAMClock),
+							.AWidth(				DDRAWidth_Top))
+				mig(		.UserClock(				ORAMClock), 
+							.UserReset(				ORAMReset), 
+                            
+							.DRAMCommand(			DDR3SDRAM_Command),
+							.DRAMAddress(			DDR3SDRAM_Address), 
+							.DRAMCommandValid(		DDR3SDRAM_CommandValid),  
+							.DRAMCommandReady(		DDR3SDRAM_CommandReady), 
+							.DRAMReadData(			DDR3SDRAM_ReadData),  
+							.DRAMReadDataValid(		DDR3SDRAM_DataOutValid), 
+							.DRAMWriteData(			DDR3SDRAM_WriteData),  
+							.DRAMWriteMask(			DDR3SDRAM_WriteMask),  
+							.DRAMWriteDataValid(	DDR3SDRAM_DataInValid),  
+							.DRAMWriteDataReady(	DDR3SDRAM_DataInReady), 
 							
-							// DRAM Controller <-> ORAM Controller 
-							.app_cmd(				DDR3SDRAM_Command_MIG),
-							.app_addr(				DDR3SDRAM_Address_MIG),
-							.app_en(				DDR3SDRAM_CommandValid_MIG),
-							.app_rdy(				DDR3SDRAM_CommandReady_MIG),
+							.sys_clk_p(				sys_clk_p),  
+							.sys_clk_n(				sys_clk_n),  
+							.sys_rst(				sys_rst), 
+							.ddr3_dq(				ddr3_dq),  
+							.ddr3_dqs_n(			ddr3_dqs_n),  
+							.ddr3_dqs_p(			ddr3_dqs_p), 			
+							.ddr3_addr(				ddr3_addr),  
+							.ddr3_ba(				ddr3_ba), 
+							.ddr3_ras_n(			ddr3_ras_n),  
+							.ddr3_cas_n(			ddr3_cas_n), 
+							.ddr3_we_n(				ddr3_we_n),  
+							.ddr3_reset_n(			ddr3_reset_n), 
+							.ddr3_ck_p(				ddr3_ck_p),  
+							.ddr3_ck_n(				ddr3_ck_n), 
+							.ddr3_cke(				ddr3_cke),  
+							.ddr3_cs_n(				ddr3_cs_n), 
+							.ddr3_dm(				ddr3_dm),  
+							.ddr3_odt(				ddr3_odt), 
+							
+							.CalibrationComplete(	DRAMCalibrationComplete));
 
-							.app_rd_data(			DDR3SDRAM_ReadData_MIG),
-							.app_rd_data_end(		), // useless?
-							.app_rd_data_valid(		DDR3SDRAM_DataOutValid_MIG),
-														
-							.app_wdf_data(			DDR3SDRAM_WriteData_MIG),
-							.app_wdf_mask(			DDR3SDRAM_WriteMask_MIG), // this is synchronous to data interface
-							.app_wdf_end(			DDR3SDRAM_DataInValid_MIG), // since DDR3 BL = 8, each 512b data chunk is the "end" of that burst
-							.app_wdf_wren(			DDR3SDRAM_DataInValid_MIG),
-							.app_wdf_rdy(			DDR3SDRAM_DataInReady_MIG),
-
-							.app_sr_req(			1'b0),
-							.app_ref_req(			1'b0),
-							.app_zq_req(			1'b0),
-							.app_sr_active(			), // not connected
-							.app_ref_ack(			), // not connected
-							.app_zq_ack(			)); // not connected
-	`endif
-	
 	//------------------------------------------------------------------------------
 endmodule
 //------------------------------------------------------------------------------
